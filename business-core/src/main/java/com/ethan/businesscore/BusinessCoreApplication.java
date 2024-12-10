11a651784a5dd252a2048bd0e0a76e1dfaf4fd36
ethan <1625650213@qq.com>
2024-12-10 18:49:18 +0800

    梳理springboot结构，重构项目结构

---
+++ business-core/src/main/java/com/ethan/businesscore/BusinessCoreApplication.java
@@ -0,0 +1,12 @@
+package com.ethan.businesscore;
+
+import org.springframework.boot.SpringApplication;
+
+//@SpringBootApplication
+public class BusinessCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessCoreApplication.class, args);
+    }
+
+}
